c97eafc82e742602fae16fc9dde8b6101c6125e4
ihor <ihor.bielchenko@drivedatum.com>
2020-04-03 20:46:59 +0300

    Switch examle

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 
-let city = null;
+const date = new Date();
+const currentMinutes = date.getMinutes();
 
-console.log(typeof city);
+const inputData = prompt('Введите дату:');
+
+console.log(inputData);
+
+switch (currentMinutes) {
+	case 44:
+	case 47:
+		console.log('Это 44!');
+		break;
+
+	case 45:
+		console.log('Это 45!');
+		break;
+
+	case 50:
+		console.log('Это 50!');
+		break;
+
+	default:
+		console.log('Все остальное');
+		break;
+}
