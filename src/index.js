f5f9faae54846c25c5ca2a2b82eb274f07a528c8
Nat Sjm <dlyagame78@gmail.com>
2020-04-05 11:45:14 +0300

    timeScript -add changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+//Для тестирования
+//const date = new Date(2020, 3, 5, 9, 0, 42);
 const date = new Date();
-const userEnter = prompt("Введите минуты или время");
-
+const userEnter = prompt("Введите минуты, или время, или дату, или время и дату в формате '15.12 14|00|30'");
 
 
 const currentMinutes = date.getMinutes();
@@ -8,107 +9,89 @@ const currentSeconds = date.getSeconds();
 const currentHours = date.getHours();
 const currentDate = date.getDate();
 const currentMonth = date.getMonth();
-const currentYear = date.getFullYear();
 
-let date2 = new Date(currentYear, currentMonth, currentDate + 1);
-let diff = Math.round((date2 - date) / 1000);
+//console.log([currentMonth, currentDate, currentHours, currentMinutes, currentSeconds]);
 
 let alertMessage = '';
 
-if (userEnter == parseInt(userEnter, 10)) {
-    let userMinutes = +userEnter;
-    if (currentMinutes === userMinutes) {
-        alertMessage = `${userMinutes} минут!`;
-    } else if ((functionTen())) {
-        alertMessage = 'Количество минут больше или равно 35 и кратно 10!'
-    } else if (diff === 1) {
-        alertMessage = 'Последняя секунда суток';
-    } else if ((currentMinutes === 59) && (currentSeconds <= 45)) {
-        alertMessage = 'Последние секунды часа';
-    }else{
-        alertMessage = 'Соответствия по минутам не найдено';
-    }
-} else {
-    let timeArray = userEnter.split(':');
-    console.log(timeArray);
-    if(timeArray.length) {
-        let userHours = timeArray[0];
-        let userMinutes = timeArray[1];
-        let userSeconds = timeArray[2];
-        if (userSeconds) {
-            if ((userHours == currentHours) && (userMinutes == currentMinutes) && (userSeconds == currentSeconds)) {
-                alertMessage = `${userHours} часов ${userMinutes} минут ${userSeconds} секунд`
+//пользователь ввел минуты
+if (userEnter) {
+    if (userEnter == parseInt(userEnter, 10)) {
+        let userMinutes = +userEnter;
+        //4 (минут number) ( ===)
+        if ((userMinutes === 4) && (currentMinutes === 4)) {
+            alertMessage = `Четыре минуты!`;
+            // 59 (минут number) (  первые 45 сек после старта 59й минуты)
+        } else if ((userMinutes === 59) && (currentMinutes === 59) && (currentSeconds <= 45)) {
+            alertMessage = 'Мы приближаемся к окончанию часа';
+            // 35 (минут number) ( >= каждые 10 мин, которые блоьше 35, напр. 45, 55)
+        } else if ((functionTen(userMinutes))) {
+            alertMessage = `Количество минут равно ${userMinutes} или ${userMinutes} + 10!`;
+        } else {
+            alertMessage = 'Соответствия по минутам не найдены';
+        }
+    } else {//другой формат данных, один из допустимых
+        // пользователь вводит данные в формате с ':', проверяем текущее время на соответствие введенной строке
+        let timeArray = userEnter.split(':');
+        if ((timeArray.length === 2) || (timeArray.length === 3)) {
+            let [userHours, userMinutes, userSeconds] = timeArray;
+            if ((userHours == currentHours) && (userMinutes == currentMinutes)) {
+                alertMessage = userSeconds ? `${userHours} часов ${userMinutes} минут ${userSeconds} секунд` :
+                    `${userHours} часов ${userMinutes} минут`;
             }
         } else {
-            if ((userHours == currentHours) && (userMinutes == currentMinutes)) {
-                alertMessage = `${userHours} часов ${userMinutes} минут`;
+            if (userEnter.match(/\./g)) {
+                // пользователь ввел данные в  формате "15.12 14|00|30"
+                if ((userEnter.match(/\|/g) && (userEnter.match(/\|/g).length === 2))) {
+                    let timeArray2 = userEnter.split('|');
+                    let [, userMinutes, userSeconds] = timeArray2;
+                    // каждые 12 сек после 30 секунды, и до старта 1й минуты
+                    if (userMinutes == 0) {
+                        if (functionTwelve(userSeconds)) {
+                            alertMessage = 'Вам удивительно повезло! Магия числа 12 принесла удачу';
+                        }
+                    }
+                } else {
+                    // пользователь ввел данные в  формате "15.12", проверяем на соответствие сегодняшней дате
+                    let timeArray3 = userEnter.split('.');
+                    let [userDate, userMonth] = timeArray3;
+                    if ((userDate == currentDate) && (userMonth == (currentMonth + 1))) {
+                        alertMessage = `Сегодня ${userEnter}`;
+                    }
+                }
             }
         }
-    } else {
-        if ((currentDate === 14) && (currentMonth === 0)) {
-            alertMessage = 'Сегодня 14.01!'
-        } else if ((currentDate === 15) && (currentMonth === 11) && (functionTwelve())) {
-            alertMessage = 'Вам сказочно повезло!'
-        }
     }
 }
 
-
-function functionTen() {
+function functionTen(userMinutes) {
     let result = false;
-    for (let i = 35; i < 59; i = i + 10) {
+    for (let i = userMinutes; i < 59; i = i + 10) {
         if (currentMinutes === i) {
-            // console.log(i);
             result = true;
             break;
         }
     }
-
     return result;
 }
 
 
-function functionTwelve() {
+function functionTwelve(userSeconds) {
     let result = false;
-    for (let i = 30; i >= 1; i = i - 12) {
-
-        if (currentSeconds === i) {
-            console.log(i);
-            result = true;
-            break;
+    if (currentMinutes === 0) {
+        for (let i = +userSeconds; i <= 59; i = i + 12) {
+            if (currentSeconds === i) {
+                result = true;
+                break;
+            }
         }
-
     }
-
     return result;
 }
 
-
-//console.log([currentYear, currentMonth, currentDate, currentHours, currentMinutes, currentSeconds]);
-if(!alertMessage){
-    alertMessage = 'Не повезло';
+//Не нашло соответствий
+if (!alertMessage) {
+    alertMessage = 'Нет совпадений';
 }
-alert(alertMessage);
 
-// // const inputData = prompt('Введите дату:');
-// //
-// // console.log(inputData);
-// //
-// // switch (currentMinutes) {
-// // 	case 44:
-// // 	case 47:
-// // 		console.log('Это 44!');
-// // 		break;
-// //
-// // 	case 45:
-// // 		console.log('Это 45!');
-// // 		break;
-// //
-// // 	case 50:
-// // 		console.log('Это 50!');
-// // 		break;
-// //
-// // 	default:
-// // 		console.log('Все остальное');
-// // 		break;
-// // }
+alert(alertMessage);
