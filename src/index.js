0a8109b0479d014b56238d4835ee9564a135d741
ihor <ihor.bielchenko@drivedatum.com>
2020-04-08 20:38:44 +0300

    Array functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 
-console.log('aaaaaaaaaaaaaaaa', console);
+const arr = [
+	110,
+	3,
+	1,
+	'qwjfie',
+	2,
+	18,
+	24,
+	4,
+	13,
+];
+const notSorts = [];
+
+const func = (prevValue, nextValue) => {
+	if (prevValue === nextValue) {
+		return 0;
+	}
+	if (prevValue < nextValue) {
+		return -1;
+	}
+	if (prevValue > nextValue) {
+		return 1;
+	}
+};
+
+const sortNumbers = arr
+	.filter((value) => {
+		if (typeof value === 'number') {
+			return true;
+		}
+		return false;
+	})
+	.sort(func);
+
+console.log('lqwjfijwq', sortNumbers.concat(notSorts));
