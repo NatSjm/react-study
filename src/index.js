adae76f6c9b22c8830663de73aaa65eceda970fa
ihor <ihor.bielchenko@drivedatum.com>
2020-04-08 15:26:28 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
 
-let i;
-
-i = 20;
-while (i > 0) {
-	console.log('WHILE TRUE', i);
-	i--;
-}
-
-for (i = 10; i > 0; i--) {
-	console.log('FOR TRUE', i);
-}
-
-let value;
-const obj = [
-	1,
-	2,
-	'stroka',
-];
-
-for (value of obj) {
-	console.log('keeeeeeeeeeey', value);
-}
+console.log('aaaaaaaaaaaaaaaa', console);
