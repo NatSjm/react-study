4673e306e5801bcd764b7739b460d00ca9fb7f1b
ihor <ihor.bielchenko@drivedatum.com>
2020-04-26 19:15:57 +0300

    Slider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Slider from 'components/Slider';
+import Slider, {
+	Slide,
+} from 'components/Slider';
 
 // const names = [
 // 	'Account',
@@ -7,9 +9,27 @@ import Slider from 'components/Slider';
 
 
 class App extends React.Component {
-	render = () => {
-		return <Slider>
+	static defaultProps = {
+		data: [{
+			id: 1,
+			text: 'oewjojg',
+			color: 'red'
+		}, {
+			color: 'blue'
+		}, {
+			color: 'green'
+		}]
+	};
 
+	render = () => {
+		const { data = [] } = this.props;
+
+		return <Slider>
+			{data.map((item, i) => {
+				return <Slide { ...item } key={i}>
+					<h1>{i}</h1>
+				</Slide>
+			})}
 		</Slider>;
 	};
 };
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,32 +1,56 @@
 import React from 'react';
 import Button from 'components/Button';
 import Wrapper from './Wrapper.jsx';
-import Slide from './Slide.jsx';
 import Row from './Row.jsx';
 
 class Slider extends React.PureComponent {
 	state = {
 		x: 0,
 	};
+	enableClick = true;
 
 	onPrev = (e) => {
 		const currentTarget = e.currentTarget;
 
-		this.setState(({ x }) => ({
-			x: x + currentTarget.nextSibling.clientWidth,
-		}));
+		if (this.enableClick === true) {
+			this.enableClick = false;
+			this.setState((currentState) => {
+				return currentState.x < 0 
+					? {
+						x: currentState.x + currentTarget.nextSibling.clientWidth,
+					}
+					: currentState;
+			});
+			setTimeout(() => {
+				this.enableClick = true;
+			}, 800);
+		}
 	};
 
 	onNext = (e) => {
 		const currentTarget = e.currentTarget;
 
-		this.setState(({ x }) => ({
-			x: x - currentTarget.previousSibling.clientWidth,
-		}));
+		if (this.enableClick === true) {
+			this.enableClick = false;
+			this.setState((currentState) => {
+				const newValue = currentState.x - currentTarget.previousSibling.clientWidth;
+				const rowWidth = -currentTarget.previousSibling.childNodes[0].clientWidth;
+
+				return newValue > rowWidth
+					? {
+						x: newValue,
+					}
+					: currentState;
+			});
+			setTimeout(() => {
+				this.enableClick = true;
+			}, 800);
+		}
 	};
 
 	render = () => {
 		const { x } = this.state;
+		const { children } = this.props;
 
 		return <React.Fragment>
 			<Button onClick={this.onPrev}>
@@ -34,9 +58,7 @@ class Slider extends React.PureComponent {
 			</Button>
 			<Wrapper>
 				<Row x={x}>
-					<Slide color="red" />
-					<Slide color="green" />
-					<Slide color="blue" />
+					{children}
 				</Row>
 			</Wrapper>
 			<Button onClick={this.onNext}>
