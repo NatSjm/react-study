140c650959a5f73ad9fb897f10a3acae14173239
ihor <ihor.bielchenko@drivedatum.com>
2020-04-29 16:32:11 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,12 @@ class App extends React.Component {
 			<Block title={<Text>First</Text>}>
 				11111111111111111
 			</Block>
+			<Block title="Second">
+				2222222222222222
+			</Block>
+			<Block title="Three">
+				3333333333333333
+			</Block>
 		</Tabs>;
 	};
 };
