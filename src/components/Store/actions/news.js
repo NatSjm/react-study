62e29134abfff7e98b2f165738d033d6ee6f3bde
ihor <ihor.bielchenko@drivedatum.com>
2020-05-08 18:27:47 +0300

    test

--- src/components/Store/actions/news.js
+++ src/components/Store/actions/news.js
@@ -1,13 +1,10 @@
+import axios from 'axios';
 
-export const get = () => (dispatch) => {
-	const response = fetch('http://0.0.0.0:3006/news');
-
-	response.then((data) => {
-		data.text().then((data) => {
-			// dispatch({
-			// 	type: '',
-			// 	payload: data,
-			// });
-		});
+export const get = () => async (dispatch) => {
+	const { data } = await axios.get('http://127.0.0.1:3006/news');
+	
+	dispatch({
+		type: '',
+		payload: data,
 	});
 };
